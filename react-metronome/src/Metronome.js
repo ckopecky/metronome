b2b9e03d9c953da2be3ed2721e35f5e7b79838b9
ckopecky <ckopecky@github.com>
2019-01-15 14:03:24 -0800

    create-react-app bare bones of app created

---
+++ react-metronome/src/Metronome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./Metronome.css";
+
+class Metronome extends Component {
+  render() {
+    return (
+      <div className="Metronome">
+        hi
+      </div>
+    );
+  }
+}
+
+export default Metronome;
